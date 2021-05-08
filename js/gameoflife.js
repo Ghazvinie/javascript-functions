643a985e8400ff4676a92e9632cdf7bb93de95b4
Daniel Ghazvinie <danielghazvinie@gmail.com>
2021-05-08 15:02:39 +0100

    step 4

--- js/gameoflife.js
+++ js/gameoflife.js
@@ -3,7 +3,7 @@ function seed() {
 }
 
 function same([x, y], [j, k]) {
- return x === j && y === k;
+  return x === j && y === k;
 }
 
 // The game state to search for `cell` is passed as the `this` value of the function.
@@ -15,70 +15,86 @@ const printCell = (cell, state) => {
   return contains.call(state, cell) ? '\u25A3' : '\u25A2';
 };
 
-const corners = (state = []) => {};
+const corners = (state = []) => {
+  if (state.length === 0) {
+    return {
+      topRight: [0, 0],
+      bottomLeft: [0, 0]
+    };
+  }
 
-const printCells = (state) => {};
+  const xCoords = state.map(([a,b]) => a);
+  const yCoords = state.map(([a,b]) => b);
 
-const getNeighborsOf = ([x, y]) => {};
+  return {
+    topRight: [Math.max(...xCoords), Math.max(...yCoords)],
+    bottomLeft: [Math.min(...yCoords), Math.min(...yCoords)]
+  };
 
-const getLivingNeighbors = (cell, state) => {};
+};
 
-const willBeAlive = (cell, state) => {};
+const printCells = (state) => { };
 
-const calculateNext = (state) => {};
+const getNeighborsOf = ([x, y]) => { };
 
-const iterate = (state, iterations) => {};
+const getLivingNeighbors = (cell, state) => { };
 
-const main = (pattern, iterations) => {};
+const willBeAlive = (cell, state) => { };
+
+const calculateNext = (state) => { };
+
+const iterate = (state, iterations) => { };
+
+const main = (pattern, iterations) => { };
 
 const startPatterns = {
-    rpentomino: [
-      [3, 2],
-      [2, 3],
-      [3, 3],
-      [3, 4],
-      [4, 4]
-    ],
-    glider: [
-      [-2, -2],
-      [-1, -2],
-      [-2, -1],
-      [-1, -1],
-      [1, 1],
-      [2, 1],
-      [3, 1],
-      [3, 2],
-      [2, 3]
-    ],
-    square: [
-      [1, 1],
-      [2, 1],
-      [1, 2],
-      [2, 2]
-    ]
-  };
-  
-  const [pattern, iterations] = process.argv.slice(2);
-  const runAsScript = require.main === module;
-  
-  if (runAsScript) {
-    if (startPatterns[pattern] && !isNaN(parseInt(iterations))) {
-      main(pattern, parseInt(iterations));
-    } else {
-      console.log("Usage: node js/gameoflife.js rpentomino 50");
-    }
+  rpentomino: [
+    [3, 2],
+    [2, 3],
+    [3, 3],
+    [3, 4],
+    [4, 4]
+  ],
+  glider: [
+    [-2, -2],
+    [-1, -2],
+    [-2, -1],
+    [-1, -1],
+    [1, 1],
+    [2, 1],
+    [3, 1],
+    [3, 2],
+    [2, 3]
+  ],
+  square: [
+    [1, 1],
+    [2, 1],
+    [1, 2],
+    [2, 2]
+  ]
+};
+
+const [pattern, iterations] = process.argv.slice(2);
+const runAsScript = require.main === module;
+
+if (runAsScript) {
+  if (startPatterns[pattern] && !isNaN(parseInt(iterations))) {
+    main(pattern, parseInt(iterations));
+  } else {
+    console.log("Usage: node js/gameoflife.js rpentomino 50");
   }
-  
-  exports.seed = seed;
-  exports.same = same;
-  exports.contains = contains;
-  exports.getNeighborsOf = getNeighborsOf;
-  exports.getLivingNeighbors = getLivingNeighbors;
-  exports.willBeAlive = willBeAlive;
-  exports.corners = corners;
-  exports.calculateNext = calculateNext;
-  exports.printCell = printCell;
-  exports.printCells = printCells;
-  exports.startPatterns = startPatterns;
-  exports.iterate = iterate;
-  exports.main = main;
+}
+
+exports.seed = seed;
+exports.same = same;
+exports.contains = contains;
+exports.getNeighborsOf = getNeighborsOf;
+exports.getLivingNeighbors = getLivingNeighbors;
+exports.willBeAlive = willBeAlive;
+exports.corners = corners;
+exports.calculateNext = calculateNext;
+exports.printCell = printCell;
+exports.printCells = printCells;
+exports.startPatterns = startPatterns;
+exports.iterate = iterate;
+exports.main = main;
